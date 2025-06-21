a2827001885539bf56fc0e4053894c10d8755b8d
kindsenior <kunio.einstein@gmail.com>
2025-06-21 20:35:04 +0900

    support genesis terrain branch (master latest)

--- inference_tutorial/scripts/dump_training_data.py
+++ inference_tutorial/scripts/dump_training_data.py
@@ -10,7 +10,7 @@ import torch
 import genesis as gs
 
 # add Genesis locomotion example to path
-sys.path.append(os.environ["HOME"] + "/genesis_ws/Genesis/examples/locomotion")
+sys.path.append(os.environ["HOME"] + "/genesis_ws/venv_genesis/src/genesis-world/examples/locomotion")
 from go2_env import Go2Env
 from rsl_rl.runners import OnPolicyRunner
 
@@ -19,24 +19,15 @@ def main():
     parser.add_argument("-e", "--exp_name", type=str, default="go2-walking")
     parser.add_argument("-l", "--log_dir", type=str, required=True, help='Path to logs/xxx directory (contains cfgs.pkl)')
     parser.add_argument("--ckpt", type=int, default=100)
-    parser.add_argument("-d", "--device", type=str, required=True, help='Calculation decice (cpu or cuda)')
     args = parser.parse_args()
 
     log_dir = args.log_dir
     cfgs_path = os.path.join(args.log_dir, "cfgs.pkl")
-    if args.device == "cpu":
-        tensor_device = gs.cpu
-    elif args.device == "gpu":
-        tensor_device = gs.gpu
-    else:
-        print(f"Not supported device: {args.device}")
-        return
 
     print(f"Loading: {log_dir}")
     print(f"Loading: {cfgs_path}")
-    print(f"Device:  {tensor_device}")
 
-    gs.init(backend=tensor_device)
+    gs.init()
 
     with open(cfgs_path, "rb") as f:
         env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg = pickle.load(f)
@@ -49,10 +40,9 @@ def main():
         reward_cfg=reward_cfg,
         command_cfg=command_cfg,
         show_viewer=True,
-        device=args.device,
     )
 
-    runner = OnPolicyRunner(env, train_cfg, log_dir, device=args.device)
+    runner = OnPolicyRunner(env, train_cfg, log_dir, device=gs.device)
     resume_path = os.path.join(log_dir, f"model_{args.ckpt}.pt")
     runner.load(resume_path)
 
@@ -62,7 +52,7 @@ def main():
     model.eval()
 
     # generate and set a dummy input
-    example_obs = torch.randn(1, obs.shape[1], device=args.device)
+    example_obs = torch.randn(1, obs.shape[1], device=gs.device)
     traced_model = torch.jit.trace(model, example_obs)
 
     policy_path = os.path.join(log_dir, f"policy_traced.pt")
